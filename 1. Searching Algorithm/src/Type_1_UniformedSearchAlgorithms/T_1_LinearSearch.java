89b33f3df87abba4695750ef9686edae0b1a31c3
Amanjeet Kumar <amanjeet.kumar@cogoport.com>
2024-07-25 15:44:17 +0530

    done with initial set up

---
+++ 1. Searching Algorithm/src/Type_1_UniformedSearchAlgorithms/T_1_LinearSearch.java
@@ -0,0 +1,12 @@
+package Type_1_UniformedSearchAlgorithms;
+
+public class T_1_LinearSearch {
+    public static void main(String[] args) {
+
+    }
+
+    public int linearSearch(int[] arr, int target) {
+
+        return target;
+    }
+}
---
+++ 2. Sorting Algorithms/src/Type_1_ComparisonBasedSortingAlgorithms/T_1_BubbleSort.java
@@ -0,0 +1,13 @@
+package Type_1_ComparisonBasedSortingAlgorithms;
+
+public class T_1_BubbleSort {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] bubbleSort(int[] arr) {
+
+
+        return arr;
+    }
+}
